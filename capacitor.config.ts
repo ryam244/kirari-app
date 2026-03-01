0c871395bb94b1bf5cd9a4eca8764d1d3703242c
ryam244 <ta06110611@gmail.com>
2026-03-01 20:58:33 +0900

    TF

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.kirari.app",
-  appName: "キラリ",
+  appId: "com.divme.kirari",
+  appName: "kirariapp",
   webDir: "out",
   plugins: {
     SplashScreen: {
